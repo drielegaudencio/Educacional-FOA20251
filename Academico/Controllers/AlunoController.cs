af0e50ce2a445d94f1e03583e723efc1697eed8c
drielegaudencio <andrielegaudencio@gmail.com>
2025-03-11 21:23:53 -0300

    Criado o Edit e as Data Anotations

--- Academico/Controllers/AlunoController.cs
+++ Academico/Controllers/AlunoController.cs
@@ -6,8 +6,24 @@ namespace Academico.Controllers
 {
     public class AlunoController : Controller
     {
-        private static List<Aluno> alunos=new List<Aluno>();
+        private static List<Aluno> alunos=new List<Aluno>()
+        {
+            new Aluno()
+            {
+                AlunoID = 1,
+                Nome = "Aluno Teste",
+                Email = "teste@mail.com",
+                Telefone = "33499662",
+                Endereco = "Rua Vinte e Um",
+                Complemento = "Casa 2",
+                Bairro = "Roma",
+                Municipio = "Volta Redonda",
+                Uf = "AC",
+                Cep = "27211-800"
 
+            }
+    };
+        
         public IActionResult Create()
         {
             return View();
@@ -16,11 +32,24 @@ namespace Academico.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken] //EVITA QUE VENHA DADOS DE OUTROS LUGARES QUE NAAO SEJAM DA PÁGINA OFICIAL, pois ele verifica se veio da página correta
 
-        public IActionResult Create(Aluno aluno)
+        public IActionResult Create([Bind("Nome","Email","Telefone","Endereço","Complemento","Bairro","Municipio","Uf","Cep")]Aluno aluno)
         {
-            alunos.Add(aluno);
-            return RedirectToAction("index");
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    aluno.AlunoID = alunos.Count + 1;
+                    alunos.Add(aluno);
+                    return RedirectToAction("Index");
+                }
+                return View(aluno);
+            }
+            catch
+            {
+                return View(aluno);
+            }
         }
+        
         public IActionResult Edit(int id)
         {
             var aluno = alunos.FirstOrDefault( a => a.AlunoID == id);
@@ -29,6 +58,14 @@ namespace Academico.Controllers
                 return NotFound();
             }
             return View(aluno);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Aluno aluno)
+        {
+            alunos.Remove(alunos.Where(i => i.AlunoID == aluno.AlunoID).First());
+            alunos.Add(aluno);
+            return RedirectToAction("Index");
         }
         public IActionResult Index()
         {
--- Academico/Models/Aluno.cs
+++ Academico/Models/Aluno.cs
@@ -1,16 +1,44 @@
-﻿namespace Academico.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academico.Models
 {
     public class Aluno
     {
+        [Required]
         public int AlunoID { get; set; }
-        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Nome é obrigatório.")]
+        public required string Nome { get; set; }
+
+        [Display(Name ="E-mail")]
+        [Required(ErrorMessage = "E-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "Endereço E-mail inválido")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Telefone é obrigatório.")]
+        [MaxLength(11, ErrorMessage = "Telefone deve conter apenas 11 dígitos.")]
         public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "Endereço é obrigatório.")]
+        [Display(Name = "Endereço")]
         public string Endereco { get; set; }
+        
         public string Complemento { get; set; }
+
+        [Required(ErrorMessage = "Bairro é obrigatório.")]
         public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "Município é obrigatório.")]
+        [Display(Name = "Município")]
         public string Municipio { get; set; }
+
+        [Required(ErrorMessage = "UF é obrigatório.")]
+        [Display(Name = "UF")]
         public string Uf { get; set; }
+
+        [Required(ErrorMessage = "CEP é obrigatório.")]
+        [MaxLength(8, ErrorMessage = "CEP deve conter apenas 8 dígitos.")]
+        [Display(Name = "CEP")]
         public string Cep { get; set; }
     }
 }
